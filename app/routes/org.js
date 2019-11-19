b7d1fc1bd23da5b32107c333a601df5a412623e0
David Tang <dtang@auditboard.com>
2019-11-18 20:31:22 -0800

    the model hook

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class OrgsRoute extends Route {
-  model(params) {
-    console.log(params);
-    return params.organization;
+  async model(params) {
+    const response = await fetch(`https://api.github.com/orgs/${params.organization}/members`)
+    return response.json();
   }
 }
